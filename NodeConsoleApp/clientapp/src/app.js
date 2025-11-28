6d832c4c4ccd6ed425f27f7d967fb1c567bbf3dd
Keith Humphrey <humphrey.keith@gmail.com>
2025-11-28 08:39:26 +0000

    28-11-2025

---
+++ NodeConsoleApp/clientapp/src/app.js
@@ -0,0 +1,36 @@
+﻿import angular from 'angular';
+import 'angular-animate';
+import 'angular-aria';
+import 'angular-material';
+import 'angular-messages';
+
+angular.module('mainApp', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages'])
+
+    .controller('MainController', function ($scope, $mdSidenav, $mdDialog) {
+
+        $scope.menuItems = [
+            { name: 'Home', icon: 'bi-house-fill' },
+            { name: 'Customers', icon: 'bi-person-fill' },
+            { name: 'Reports', icon: 'bi-file-earmark-text-fill' },
+            { name: 'Settings', icon: 'bi-wrench' }
+        ];
+
+        $scope.showDialog = function () {
+
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .title('Alert')
+                    .textContent('ℹ️ Information')
+                    .ok('Close')
+                );
+        };
+
+        $scope.toggleMenu = function () {
+            $mdSidenav('left').toggle();
+        };
+
+        $scope.closeMenu = function () {
+            $mdSidenav('left').close();
+        };
+
+    });
